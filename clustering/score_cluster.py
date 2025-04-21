7782c59ee7148231398395dd00c20228acca158e
master <quinndu0@gmail.com>
2025-04-21 14:40:13 +0000

    feat: add script for fast start and cleaning local/HDFS data

--- clustering/score_cluster.py
+++ clustering/score_cluster.py
@@ -1,11 +1,25 @@
-from pyspark.ml.clustering import KMeans
+from pyspark.ml.clustering import KMeans, KMeansModel
 from pyspark.ml.feature import VectorAssembler
 
+from config.feature_select import get_score_features
+
+
 def run(df, config):
-    assembler = VectorAssembler(inputCols=["Total_Score"], outputCol="score_vector")
+    assembler = VectorAssembler(inputCols=get_score_features(), outputCol="score_vector")
     df = assembler.transform(df)
 
     kmeans = KMeans(k=3, featuresCol="score_vector", predictionCol="score_cluster")
     model = kmeans.fit(df)
     model.write().overwrite().save(config['model']['score_cluster'])
     return model.transform(df)
+
+
+def predict_with_score_model(df, config):
+    model_path = config['model']['score_cluster']
+    model = KMeansModel.load(model_path)
+
+    assembler = VectorAssembler(inputCols=["Total_Score"], outputCol="score_vector")
+    df = assembler.transform(df)
+
+    df = model.transform(df)
+    return df
